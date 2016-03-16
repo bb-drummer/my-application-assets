17e7907df068c09aede32e841be089999223b38a
Björn Bartels <bartels@dragon-projects.net>
2016-03-17 00:50:09 +0100

    update css, js, assets

--- dist/myapplication.js
+++ dist/myapplication.js
@@ -14,6 +14,7 @@
 !function($) {
 "use strict";
 
+
 var MYAPPLICATION_VERSION = '';
 
 // Global [MyApplication] object
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browser = require('browser-sync');
 var requireDir = require('require-dir');
+var sequence = require('run-sequence');
 var exec = require('child_process').execSync;
 var port = process.env.SERVER_PORT || 3000;
 
@@ -9,10 +10,6 @@ requireDir('./gulp');
 
 //Writes a commit with the changes to the version numbers
 gulp.task('commit', function(cb) {
-
-  gulp.src('dist/**/*')
-    .pipe(gulp.dest('../application-base/public/assets/dist'));
-
   exec('git add .');
   exec('git commit -m "update css, js, assets"');
   exec('git push');
@@ -20,8 +17,19 @@ gulp.task('commit', function(cb) {
 });
 
 
+//Writes a commit with the changes to the version numbers
+gulp.task('devcopy', function(cb) {
+
+  gulp.src('dist/**/*')
+    .pipe(gulp.dest('../application-base/public/application-assets/dist'));
+
+});
+
+
 // Builds the documentation and framework files
-gulp.task('build', ['clean', 'copy', 'docs:all', 'sass', 'javascript', 'deploy']);
+gulp.task('build', function (cb) {
+	sequence('clean', 'copy', 'docs:all', 'sass', 'javascript', 'deploy', 'devcopy', cb);
+});
 
 // Starts a BrowerSync instance
 gulp.task('serve', ['build'], function(){
@@ -29,14 +37,15 @@ gulp.task('serve', ['build'], function(){
 });
 
 //Runs all of the above tasks and then waits for files to change
-gulp.task('watch', ['build'], function() {
-  gulp.watch('src/docs/**/*', ['docs', browser.reload]);
-  gulp.watch(['src/docs/layout/*.html', 'src/docs/partials/*.html'], ['docs:all', browser.reload]);
-  gulp.watch('src/scss/**/*', ['sass', browser.reload]);
-  gulp.watch('src/docs/assets/scss/**/*', ['sass:docs', browser.reload]);
-  gulp.watch('src/js/**/*', ['javascript:myapplication', browser.reload]);
-  gulp.watch('src/docs/assets/js/**/*', ['javascript:docs', browser.reload]);
+gulp.task('default', ['build', 'serve'], function() {
+  gulp.watch('src/**/*', ['build']);
+  //gulp.watch('src/docs/**/*', ['docs', browser.reload]);
+  //gulp.watch(['src/docs/layout/*.html', 'src/docs/partials/*.html'], ['docs:all', browser.reload]);
+  //gulp.watch('src/scss/**/*', ['sass', browser.reload]);
+  //gulp.watch('src/docs/assets/scss/**/*', ['sass:docs', browser.reload]);
+  //gulp.watch('src/js/**/*', ['javascript:myapplication', browser.reload]);
+  //gulp.watch('src/docs/assets/js/**/*', ['javascript:docs', browser.reload]);
 });
 
 // Runs all of the above tasks and then waits for files to change
-gulp.task('default', ['build']);
+//gulp.task('default', ['build', 'watch']);
